788165489a17e2daa010dab84c437ba8b689673a
Campbell <KidzCode42@gmail.com>
2021-02-12 12:13:06 -0600

    First draft of the RegExplorer UI.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,41 @@ namespace RegExPlorer
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		Regex regex;
+		string textToMatch;
+		void CheckForMatch()
+		{
+			if (regex.IsMatch(textToMatch))
+			{
+				blueCheck.Visibility = Visibility.Visible;
+				redX.Visibility = Visibility.Collapsed;
+			}
+			else
+			{
+				blueCheck.Visibility = Visibility.Collapsed;
+				redX.Visibility = Visibility.Visible;
+			}
+		}
+		private void tbxRegExPattern_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			try
+			{
+				regex = new Regex(tbxRegExPattern.Text);
+				CheckForMatch();
+				Title ="RegEx Looks Good!";
+			}
+			catch (Exception ex)
+			{
+				Title = ex.Message;
+			}
+		}
+
+		private void tbxText_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			textToMatch = tbxText.Text;
+			CheckForMatch();
 		}
 	}
 }
